73569e825441c004c5d764271790a56bf4d90ee2
Jonathan Köre <korejonathan@gmail.com>
2021-08-24 10:33:53 +0200

    modified structure of environment class by extracting some methods from the class itself

--- src/AI/environment.py
+++ src/AI/environment.py
@@ -1,4 +1,4 @@
-from Game import model, view, controller
+from Game import model, view
 
 class Environment:
     def __init__(self, render = False):
@@ -9,32 +9,17 @@ class Environment:
 
     def step(self, action):
         self.game_model.change_direction(action)
-        reward_data_before = self.extract_reward_data()
+        reward_data_before = extract_reward_data(self.game_model)
         self.game_model.step()
-        reward_data_after = self.extract_reward_data()
+        reward_data_after = extract_reward_data(self.game_model)
         reward = calculate_reward(reward_data_before, reward_data_after)
-        state = self.extract_state()
+        state = extract_state(self.game_model)
         done = self.game_model.game_status == -1
         return state, reward, done
 
-    def extract_state(self):
-        player_body = self.game_model.player_body
-        player_head = self.game_model.player_head
-        fruit = self.game_model.fruit
-        state = [[0 for i in range(self.game_model.grid_width + 2)] for j in range(self.game_model.grid_height + 2)]
-        for pos in player_body:
-            state[pos[0] + 1][pos[1] + 1] = 1
-        state[player_head[0] + 1][player_head[1] + 1] = 2
-        state[fruit[0] + 1][fruit[1] + 1] = -1
-        return state
-
-
-    def extract_reward_data(self):
-        return RewardData(snake_length=self.game_model.snake_length, game_status=self.game_model.game_status)
-
     def reset(self):
         self.game_model.reset()
-        return self.extract_state()
+        return extract_state(self.game_model)
 
 
 
@@ -48,6 +33,31 @@ def calculate_reward(data_before: RewardData, data_after: RewardData):
     if data_after.game_status == -1:
         return -1
     return data_after.snake_length - data_before.snake_length
+
+"""
+BODY_PLANE = 0
+HEAD_PLANE = 1
+FRUIT_PLANE = 2
+"""
+
+def extract_state(game_model):
+    player_body = list(game_model.player_body)
+    player_head = list(game_model.player_head)
+    fruit = list(game_model.fruit)
+    """
+    state = [[[0 for i in range(game_model.grid_width + 2)] for j in range(game_model.grid_height + 2)]
+             for k in range(3)]
+
+    for pos in player_body:
+        state[BODY_PLANE][pos[0] + 1][pos[1] + 1] = 1
+    state[HEAD_PLANE][player_head[0] + 1][player_head[1] + 1] = 1
+    state[FRUIT_PLANE][fruit[0] + 1][fruit[1] + 1] = 1
+    """
+    return [player_head, player_body, fruit]
+
+
+def extract_reward_data(game_model):
+    return RewardData(snake_length=game_model.snake_length, game_status=game_model.game_status)
 
 
 
